47abaf70fdf05ff6dca6e0b29e1a7a81975e66b5
Md. Suman Sarkar <suman.iu32@gmail.com>
2023-09-24 23:04:50 +0600

    get api integrated

--- src/apiCalling/agencyWebSite.js
+++ src/apiCalling/agencyWebSite.js
@@ -25,3 +25,28 @@ export const GET_ALL_PROJECT_LIST = () => {
 	const url = 'api/AllProject';
 	return axios.get(url);
 };
+// get all services list
+export const GET_ALL_SERVICES_LIST = () => {
+	const url = 'api/AllService';
+	return axios.get(url);
+};
+// get  social list
+export const GET_ALL_SOCIAL_LIST = () => {
+	const url = 'api/SocialLink';
+	return axios.get(url);
+};
+// get  StatList
+export const GET_STAT_LIST = () => {
+	const url = 'api/StatList';
+	return axios.get(url);
+};
+// get  StatList
+export const GET_TEAM_LIST = () => {
+	const url = 'api/TeamList';
+	return axios.get(url);
+};
+// get  testimonialList
+export const GET_TESTIMONIAL_LIST = () => {
+	const url = 'api/TestimonialList';
+	return axios.get(url);
+};
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,109 +1,108 @@
-const Services = () => {
-    return (
-        <div>
-            <section>
-                <div className="skew skew-top mr-for-radius">
-                    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
-                        <polygon fill="currentColor" points="0 0 10 10 0 10" />
-                    </svg>
-                </div>
-                <div className="skew skew-top ml-for-radius">
-                    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
-                        <polygon fill="currentColor" points="0 10 10 0 10 10" />
-                    </svg>
-                </div>
-                <div className="py-20 bg-gray-50 radius-for-skewed">
-                    <div className="container mx-auto px-4">
-                        <div className="flex flex-wrap items-center -mx-4">
-                            <div className="mb-12 lg:mb-0 w-full lg:w-1/2 flex px-4">
-                                <div className="max-w-md">
-                                    <span className="text-green-600 font-bold">Dolor sit amet consectutar</span>
-                                    <h2 className="mb-6 text-4xl lg:text-5xl font-bold font-heading">
-                                        Build &amp; Launch without problems
-                                    </h2>
-                                    <div className="mb-6 max-w-sm">
-                                        <p className="text-gray-500 leading-loose">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            Pellentesque efficitur nisl sodales egestas lobortis.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="w-full lg:w-1/2">
-                                <div className="mb-4 flex flex-wrap items-end">
-                                    <div className="mb-4 lg:mb-0 w-full lg:w-2/3 px-3">
-                                        <img className="w-full h-32 lg:h-48 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/601441dd6dfbe10018e00c25.webp"/>
-                                    </div>
-                                    <div className="w-full lg:w-1/3 px-3">
-                                        <img className="w-full h-32 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/960x0.jpg" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="flex flex-wrap items-start">
-                                    <div className="mb-4 lg:mb-0 w-full lg:w-1/3 px-3">
-                                        <img className="w-full h-32 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/hub-01-18-projectmanagement-Hero-1200x900-software-tips.jpg" alt=""/>
-                                    </div>
-                                    <div className="w-full lg:w-2/3 px-3">
-                                        <img className="w-full h-32 lg:h-48 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/960x0.jpg" alt=""/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+import { GET_ALL_SERVICES_LIST } from '@/apiCalling/agencyWebSite';
 
-                <div className="py-20 bg-gray-50 radius-for-skewed">
-                    <div className="container mx-auto px-4">
-                        <div className="flex flex-wrap items-center -mx-4">
-                            <div className="mb-12 lg:mb-0 w-full lg:w-1/2 flex px-4">
-                                <div className="max-w-md">
-                                    <span className="text-green-600 font-bold">Dolor sit amet consectutar</span>
-                                    <h2 className="mb-6 text-4xl lg:text-5xl font-bold font-heading">
-                                        Build &amp; Launch without problems
-                                    </h2>
-                                    <div className="mb-6 max-w-sm">
-                                        <p className="text-gray-500 leading-loose">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                            Pellentesque efficitur nisl sodales egestas lobortis.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="w-full lg:w-1/2">
-                                <div className="mb-4 flex flex-wrap items-end">
-                                    <div className="mb-4 lg:mb-0 w-full lg:w-2/3 px-3">
-                                        <img className="w-full h-32 lg:h-48 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/601441dd6dfbe10018e00c25.webp"/>
-                                    </div>
-                                    <div className="w-full lg:w-1/3 px-3">
-                                        <img className="w-full h-32 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/960x0.jpg" alt=""/>
-                                    </div>
-                                </div>
-                                <div className="flex flex-wrap items-start">
-                                    <div className="mb-4 lg:mb-0 w-full lg:w-1/3 px-3">
-                                        <img className="w-full h-32 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/hub-01-18-projectmanagement-Hero-1200x900-software-tips.jpg" alt=""/>
-                                    </div>
-                                    <div className="w-full lg:w-2/3 px-3">
-                                        <img className="w-full h-32 lg:h-48 object-cover rounded" src="https://static.shuffle.dev/uploads/files/36/368971920a7783ddacbdc0de43a17896c8faed5f/960x0.jpg" alt=""/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+const getAllServices = async () => {
+	try {
+		const res = await GET_ALL_SERVICES_LIST();
+		return res.data;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
 
+const Services = async () => {
+	const services = await getAllServices();
+	console.log(services);
+	return (
+		<div>
+			<section>
+				<div className="skew skew-top mr-for-radius">
+					<svg
+						className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+						viewBox="0 0 10 10"
+						preserveAspectRatio="none">
+						<polygon fill="currentColor" points="0 0 10 10 0 10" />
+					</svg>
+				</div>
+				<div className="skew skew-top ml-for-radius">
+					<svg
+						className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+						viewBox="0 0 10 10"
+						preserveAspectRatio="none">
+						<polygon fill="currentColor" points="0 10 10 0 10 10" />
+					</svg>
+				</div>
+				{services?.map((service) => (
+					<div className="py-20 bg-gray-50 radius-for-skewed" key={service.id}>
+						<div className="container mx-auto px-4">
+							<div className="flex flex-wrap items-center -mx-4">
+								<div className="mb-12 lg:mb-0 w-full lg:w-1/2 flex px-4">
+									<div className="max-w-md">
+										<span className="text-green-600 font-bold">{service.tag || ''}</span>
+										<h2 className="mb-6 text-4xl lg:text-5xl font-bold font-heading">
+											{service.title || ''}
+										</h2>
+										<div className="mb-6 max-w-sm">
+											<p className="text-gray-500 leading-loose">{service.des || ''}</p>
+										</div>
+									</div>
+								</div>
+								<div className="w-full lg:w-1/2">
+									<div className="mb-4 flex flex-wrap items-end">
+										<div className="mb-4 lg:mb-0 w-full lg:w-2/3 px-3">
+											<img
+												className="w-full h-32 lg:h-48 object-cover rounded"
+												src={service.image1}
+											/>
+										</div>
+										<div className="w-full lg:w-1/3 px-3">
+											<img
+												className="w-full h-32 object-cover rounded"
+												src={service.image2}
+												alt=""
+											/>
+										</div>
+									</div>
+									<div className="flex flex-wrap items-start">
+										<div className="mb-4 lg:mb-0 w-full lg:w-1/3 px-3">
+											<img
+												className="w-full h-32 object-cover rounded"
+												src={service.image3}
+												alt=""
+											/>
+										</div>
+										<div className="w-full lg:w-2/3 px-3">
+											<img
+												className="w-full h-32 lg:h-48 object-cover rounded"
+												src={service.image4}
+												alt=""
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
 
-                <div className="skew skew-bottom mr-for-radius">
-                    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
-                        <polygon fill="currentColor" points="0 0 10 0 0 10" />
-                    </svg>
-                </div>
-                <div className="skew skew-bottom ml-for-radius">
-                    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
-                        <polygon fill="currentColor" points="0 0 10 0 10 10" />
-                    </svg>
-                </div>
-            </section>
-        </div>
-    );
+				<div className="skew skew-bottom mr-for-radius">
+					<svg
+						className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+						viewBox="0 0 10 10"
+						preserveAspectRatio="none">
+						<polygon fill="currentColor" points="0 0 10 0 0 10" />
+					</svg>
+				</div>
+				<div className="skew skew-bottom ml-for-radius">
+					<svg
+						className="h-8 md:h-12 lg:h-20 w-full text-gray-50"
+						viewBox="0 0 10 10"
+						preserveAspectRatio="none">
+						<polygon fill="currentColor" points="0 0 10 0 10 10" />
+					</svg>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Services;
